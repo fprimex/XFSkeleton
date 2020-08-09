1ece25cf7bfb609368e20234d9b4f8cb0205c446
Brent Woodruff <brent@fprimex.com>
2020-08-09 16:04:52 -0400

    Initial commit. Android, iOS, and MacOS projects.

---
+++ XFSkeleton.Mac/AppDelegate.cs
@@ -0,0 +1,49 @@
+﻿using AppKit;
+using Foundation;
+
+namespace XFSkeleton.Mac
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : Xamarin.Forms.Platform.MacOS.FormsApplicationDelegate
+    {
+        NSWindow window;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            window.Title = "Xamarin.Forms on Mac!";
+            window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+
+        public override NSWindow MainWindow
+        {
+            get { return window; }
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
